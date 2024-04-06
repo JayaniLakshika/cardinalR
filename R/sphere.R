4561815241bcd9c5c4ebc69ced62cc2574f7a72a
JayaniLakshika <jayanilakshika76@gmail.com>
2024-04-06 17:04:58 +1100

    Add small_big_sphere function

--- R/sphere.R
+++ R/sphere.R
@@ -44,3 +44,67 @@ sphere <- function(radius, resolution, num_noise_dims, min_noise, max_noise) {
   }
 
 }
+
+#' Generate data representing small spheres within a larger encompassing sphere with added noise.
+#'
+#' This function generates data points representing small spheres within a larger encompassing sphere
+#' and adds noise to the data if specified.
+#'
+#' @param sample_size Total number of data points to generate, should be a multiple of 13.
+#' @param num_noise_dims Number of additional noise dimensions to add to the data.
+#' @param min_noise Minimum value for the noise added to the data.
+#' @param max_noise Maximum value for the noise added to the data.
+#'
+#' @return A matrix containing the generated data points with or without added noise.
+#'
+#' @examples
+#' small_big_sphere_with_noise(sample_size = 390, num_noise_dims = 4,
+#' min_noise = -0.05, max_noise = 0.05)
+#'
+#' @export
+small_big_sphere_with_noise <- function(sample_size, num_noise_dims, min_noise,
+                                        max_noise) {
+
+  # To check that the assigned sample_size is divided by thirteen
+  if ((sample_size%%13) != 0) {
+    warning("The sample size should be a product of thirteen.")
+    small_sphere_sample_size <- floor(sample_size/13)
+
+  } else {
+    small_sphere_sample_size <- sample_size/13
+  }
+
+  m <- matrix(stats::rnorm(n = small_sphere_sample_size * 4), nrow = small_sphere_sample_size, ncol = 4)
+  d_dim_sphere <- 3 * m / sqrt(rowSums(m * m))
+
+  small_spheres <-
+    replicate(3,
+              sweep(
+                d_dim_sphere, 2,
+                stats::rnorm(n = 4, sd = 10 / sqrt(3)), `+`
+              ),
+              simplify = FALSE
+    )
+
+  # The larger encompassing sphere
+  n_big_samples <- 10 * small_sphere_sample_size
+  m <- matrix(stats::rnorm(n = n_big_samples * 4), nrow = n_big_samples, ncol = 4)
+  big_sphere <- 3 * 5 * m / sqrt(rowSums(m * m))
+
+  df <- rbind(do.call(rbind, small_spheres), big_sphere)
+
+  if (num_noise_dims != 0) {
+
+    noise_mat <- gen_noise_dims(n = dim(df)[1], num_noise_dims = num_noise_dims,
+                                min_noise = min_noise, max_noise = max_noise)
+    df <- cbind(df, noise_mat)
+
+    df
+
+  } else {
+
+    df
+
+  }
+
+}
