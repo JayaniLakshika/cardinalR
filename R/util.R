e30011a85c440fc672571ebd0491d314f9e5eeb0
thiyangt <tstalagala@gmail.com>
2025-12-16 06:39:25 +0530

    word adjustment

--- R/util.R
+++ R/util.R
@@ -88,7 +88,7 @@ gen_bkgnoise <- function(n = 500, p = 4, m = rep(0, p), s = rep(2, p)) {
   }
 
   if (!is.vector(m)) {
-    cli::cli_abort("m should be vector.")
+    cli::cli_abort("m should be a vector.")
   }
 
   if (length(m) != p) {
